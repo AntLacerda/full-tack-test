a4e05fc9380a15c5390737bdfd959770aff150ca
AntLacerda <lacerdaneto99@hotmail.com>
2025-04-18 16:45:46 -0300

    Feat: Add botão de voltar a dashboard da página de criar quadras

--- front-end/app/(main)/court/create/page.tsx
+++ front-end/app/(main)/court/create/page.tsx
@@ -1,9 +1,14 @@
 import CreateCourtForm from "@/components/dashboard/create-court-form";
+import { ArrowLeftIcon } from "@heroicons/react/24/solid"
+import Link from "next/link";
 
 export default function CreateCourt() {
     return (
         <main className="w-full flex flex-col p-6">
             <div>
+                <Link href="/dashboard">
+                    <ArrowLeftIcon className="w-6 h-6"/>
+                </Link>
                 <h1 className="text-5xl font-bold mb-3">Criar Quadra</h1>
                 <p>Preencha os dados necessários para criar uma nova quadra!</p>
             </div>
