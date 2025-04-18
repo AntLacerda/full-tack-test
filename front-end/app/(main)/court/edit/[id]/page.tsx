b71b18247b1c639d35bf4cc6ad2b8e469a17d5c5
AntLacerda <lacerdaneto99@hotmail.com>
2025-04-18 16:24:43 -0300

    Feat: Add botão de voltar a dashboard

--- front-end/app/(main)/court/edit/[id]/page.tsx
+++ front-end/app/(main)/court/edit/[id]/page.tsx
@@ -1,9 +1,14 @@
 import EditCourtForm from "@/components/dashboard/edit-court-form"
+import Link from "next/link"
+import { ArrowLeftIcon } from "@heroicons/react/24/solid"
 
 export default function EditCourt() {
     return( 
         <main className="w-full flex flex-col p-6">
             <div>
+                <Link href="/dashboard">
+                    <ArrowLeftIcon className="w-6 h-6"/>
+                </Link>
                 <h1 className="text-5xl font-bold mb-3">Editar Quadra</h1>
                 <p>Preencha os dados necessários para criar uma nova quadra!</p>
             </div>
