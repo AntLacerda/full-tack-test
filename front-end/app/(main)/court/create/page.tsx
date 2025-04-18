c03e664a0ee5335660c9320d45555235f6ba3cdd
AntLacerda <lacerdaneto99@hotmail.com>
2025-04-18 14:22:15 -0300

    Feat: Iniciando o desenvolvimento da tela de criar quadra

--- front-end/app/(main)/court/create/page.tsx
+++ front-end/app/(main)/court/create/page.tsx
@@ -1,7 +1,8 @@
 export default function CreateCourt() {
     return (
-        <div>
-            <h1 className="text-3xl font-bold">Criar Quadra</h1>
-        </div>
+        <main className="w-full flex flex-col p-6">
+            <h1 className="text-5xl font-bold mb-3">Criar Quadra</h1>
+            <p>Preencha os dados necessários para criar uma nova quadra!</p>
+        </main>
     );
 }
