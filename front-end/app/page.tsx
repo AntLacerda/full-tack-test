a12c27caf17d0e27de224bd3c37593556b0820bd
AntLacerda <lacerdaneto99@hotmail.com>
2025-04-19 10:20:06 -0300

    Fix: Ajustando layout da página de entrada

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="bg-[url('/images/home.png')] bg-cover w-full h-screen bg-center flex flex-col items-center justify-center">
-        <Link href={"/login"}>
-          <button className="bg-[#629764] text-white font-bold py-2 px-4 rounded-md mt-4 cursor-pointer mt-46">Começar</button>
+        <Link href={"/login"} className="mt-46">
+          <button className="bg-[#629764] text-white font-bold py-2 px-4 rounded-md cursor-pointer ">Começar</button>
         </Link>
     </div>
   );
