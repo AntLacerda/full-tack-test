cf2e58ee6f174e2d39b31ec242eb03107f65de1e
AntLacerda <lacerdaneto99@hotmail.com>
2025-04-19 10:17:52 -0300

    Feat: Desenvolvendo página de entrada

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold">Home</h1>
+    <div className="bg-[url('/images/home.png')] bg-cover w-full h-screen bg-center flex flex-col items-center justify-center">
+        <Link href={"/login"}>
+          <button className="bg-[#629764] text-white font-bold py-2 px-4 rounded-md mt-4 cursor-pointer mt-46">Começar</button>
+        </Link>
     </div>
   );
 }
